837004a4e3a4a42eb73b23b53f37903816e61237
Temituoyo Ugborogho <74937119+temituoyo@users.noreply.github.com>
2021-10-11 12:58:34 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 def welcome():
-    print("Hello World!")
+    while True:
+        message = input("Enter a greeting: ")
+        try:
+            if ((message == "Hi") or (message == "HI") or (message == "hi") or (message == "hI")):
+                print("Hello World!")
+            else:
+                raise Exception
+        except:
+            print("Say hi")
+        else:
+            break
 
 welcome()
